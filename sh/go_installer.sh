d01f1ead55f7042cd3cede359fb67e7daab9e218
maxim <zanoga.maxim@gmail.com>
2022-05-03 12:40:42 +0100

    add go installer for arm

---
+++ sh/go_installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ARM
+export GOLANG="$(curl -s https://go.dev/dl/ | awk -F[\>\<] '/linux-armv6l/ && !/beta/ {print $5;exit}')"
+wget https://golang.org/dl/$GOLANG
+sudo tar -C /usr/local -xzf $GOLANG
+rm $GOLANG
+unset GOLANG
+
+echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'GOPATH=$HOME/golang' >> ~/.profile
+
+source ~/.profile
+
+which go
+go version
+
+cd ~
+
+mkdir golang
+mkdir golang/src
