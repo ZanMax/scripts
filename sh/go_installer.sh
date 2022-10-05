d878b206e7a93e3e82d2412cd03d42436584a613
maxim <zanoga.maxim@gmail.com>
2022-10-04 18:41:38 -0700

    update go install script, fix arm64

--- sh/go_installer.sh
+++ sh/go_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ARM
-export GOLANG="$(curl -s https://go.dev/dl/ | awk -F[\>\<] '/linux-armv6l/ && !/beta/ {print $5;exit}')"
+export GOLANG="$(curl -s https://go.dev/dl/ | awk -F[\>\<] '/linux-arm64/ && !/beta/ {print $5;exit}')"
 wget https://golang.org/dl/$GOLANG
 sudo tar -C /usr/local -xzf $GOLANG
 rm $GOLANG
