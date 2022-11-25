ab3c6dd449891607a3d337b3672c2b57397314de
maxim <zanoga.maxim@gmail.com>
2022-11-24 22:11:09 -0800

    update script batmon

--- sh/batmon.sh
+++ sh/batmon.sh
@@ -13,17 +13,17 @@ do
     BATPERSENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
     clear
     echo $BATPERSENT
-    if [ $BATPERSENT > 90 ]; then
+    if [[ $BATPERSENT > 90 ]]; then
       brightness 0.5
-    elif [ $BATPERSENT > 70 && $BATPERSENT < 90 ]; then
+    elif [[ $BATPERSENT > 70 ]] && [[ $BATPERSENT < 90 ]]; then
       brightness 0.4
-    elif [ $BATPERSENT > 50 && $BATPERSENT < 70 ]; then
+    elif [[ $BATPERSENT > 50 ]] && [[ $BATPERSENT < 70 ]]; then
       brightness 0.3
-    elif [ $BATPERSENT > 30 && $BATPERSENT < 50 ]; then
+    elif [[ $BATPERSENT > 30 ]] && [[ $BATPERSENT < 50 ]]; then
       brightness 0.2
-    elif [ $BATPERSENT > 10 && $BATPERSENT < 30 ]; then
+    elif [[ $BATPERSENT > 10 ]] && [[ $BATPERSENT < 30 ]]; then
       brightness 0.15
-    elif [ $BATPERSENT < 10 ]; then
+    elif [[ $BATPERSENT < 10 ]]; then
       brightness 0.1
     fi
     sleep 30
