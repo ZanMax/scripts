c4ffa5a3410ed99b50acc8349383986a5cfc7a3c
maxim <zanoga.maxim@gmail.com>
2022-12-11 00:42:53 -0800

    update go install script

--- sh/go_installer.sh
+++ sh/go_installer.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
-# ARM
-export GOLANG="$(curl -s https://go.dev/dl/ | awk -F[\>\<] '/linux-arm64/ && !/beta/ {print $5;exit}')"
-wget https://golang.org/dl/$GOLANG
-sudo tar -C /usr/local -xzf $GOLANG
-rm $GOLANG
+ARCH=$(uname -m)
+url=""
+archive=""
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if [[ "$ARCH" == "x86_64" ]]; then
+    latest_version=$(wget -qO- https://go.dev/dl/ | grep -Eo '/dl/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -1)
+    url="https://go.dev${latest_version}"
+    archive=${latest_version:4}
+  elif [[ "$ARCH" == "aarch64" ]]; then
+    latest_version=$(wget -qO- https://go.dev/dl/ | grep -Eo '/dl/go([0-9\.]+)\.linux-arm64\.tar\.gz' | head -1)
+    url="https://go.dev${latest_version}"
+    archive=${latest_version:4}
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  if [[ "$ARCH" == "x86_64" ]]; then
+    latest_version=$(wget -qO- https://go.dev/dl/ | grep -Eo '/dl/go([0-9\.]+)\.darwin-amd64\.tar\.gz' | head -1)
+    url="https://go.dev${latest_version}"
+    archive=${latest_version:4}
+  elif [[ "$ARCH" == "aarch64" ]]; then
+    latest_version=$(wget -qO- https://go.dev/dl/ | grep -Eo '/dl/go([0-9\.]+)\.darwin-arm64\.tar\.gz' | head -1)
+    url="https://go.dev${latest_version}"
+    archive=${latest_version:4}
+  fi
+fi
+
+wget --quiet --continue --show-progress "${url}"
+
+# Remove Old Go
+sudo rm -rf /usr/local/go
+
+# Install New Go
+sudo tar -C /usr/local -xzf $archive
+
+rm $archive
 unset GOLANG
 
-echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-echo 'GOPATH=$HOME/golang' >> ~/.profile
+echo 'PATH=$PATH:/usr/local/go/bin' >>~/.profile
+echo 'GOPATH=$HOME/golang' >>~/.profile
 
 source ~/.profile
 
