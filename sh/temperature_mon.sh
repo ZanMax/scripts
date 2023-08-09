1a8338fef3bbb76d587873698268e07cd68666a0
Max Zanoga <zanoga.maxim@gmail.com>
2023-08-08 18:20:43 -0700

    update temperature monitoring script

--- sh/temperature_mon.sh
+++ sh/temperature_mon.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-# Get CPU temperature from the k10temp sensor
-cpu_temp=$(sensors | awk '/Tctl/ {print $2}')
+# Detect CPU Brand
+cpu_brand=$(grep -m 1 "vendor_id" /proc/cpuinfo | awk '{print $3}')
 
-# Get GPU temperature from the amdgpu sensor
-gpu_temp=$(sensors | awk '/edge/ {print $2}')
+if [[ $cpu_brand == "GenuineIntel" ]]; then
+    # Intel-based temperature extraction
+    # Get overall CPU Package temperature
+    cpu_temp=$(sensors | awk '/Package id 0:/ {print $4}')
+    
+    # Get NVME temperature
+    nvme_temp=$(sensors | awk '/Composite/ {print $2}')
+    
+    # Get PCH temperature
+    pch_temp=$(sensors | awk '/temp1:/ {print $2}' | head -1)
+    
+    echo "CPU Temperature: $cpu_temp"
+    echo "NVME Temperature: $nvme_temp"
+    echo "PCH Temperature: $pch_temp"
 
-# Get NVME temperature from the nvme-pci sensor
-nvme_temp=$(sensors | awk '/Composite/ {print $2}')
+elif [[ $cpu_brand == "AuthenticAMD" ]]; then
+    # AMD-based temperature extraction
+    # Get GPU temperature from the amdgpu sensor
+    gpu_temp=$(sensors | awk '/edge/ {print $2}')
+    
+    # Get CPU temperature from the k10temp sensor
+    cpu_temp=$(sensors | awk '/Tctl/ {print $2}')
+    
+    # Get NVME temperature from the nvme-pci sensor
+    nvme_temp=$(sensors | awk '/Composite/ {print $2}')
+    
+    echo "CPU Temperature: $cpu_temp"
+    echo "GPU Temperature: $gpu_temp"
+    echo "NVME Temperature: $nvme_temp"
 
-echo "CPU Temperature: $cpu_temp"
-echo "GPU Temperature: $gpu_temp"
-echo "NVME Temperature: $nvme_temp"
+else
+    echo "Unknown processor vendor."
+fi
